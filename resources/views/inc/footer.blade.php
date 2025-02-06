1d0bc2ed9c2bd9860432ce5a074146bd3cff02c2
Lukas <lukik.steam@gmail.com>
2025-02-06 09:26:46 +0100

    footer

---
+++ resources/views/inc/footer.blade.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        .card-1 {
+            font-family: Arial, Helvetica, sans-serif;
+            color: black;
+            
+
+            }
+
+        .card-2{
+            display: flex;
+            padding: 0,30px,0,0;
+            align-items: center;
+        }
+
+        .main-container {
+            display: flex;
+            justify-content: space-around;
+        }
+
+
+
+    </style>
+</head>
+<body>
+    <div class="main-container">
+        <div class="card-1"><!-- card-1 -->
+            <h3>O nás</h3>
+            <ul>
+                <li><a href="#">Kontakt</a></li>
+                <li><a href="#">O nás</a></li>
+                <li><a href="#">História</a></li>
+                <li><a href="#">Vedenie</a></li>
+            </ul>
+
+        </div>
+
+        <div class="card-1"><!-- card-2 -->
+            <h3>Informácie</h3>
+            <ul>
+                <li><a href="#">Kontakt</a></li>
+                <li><a href="#">O nás</a></li>
+                <li><a href="#">História</a></li>
+                <li><a href="#">Vedenie</a></li>
+        </div>
+
+        <div class="card-1"><!-- card-3 -->
+            <h3>Zaujíma vás</h3>
+            <ul>
+                <li><a href="#">Kontakt</a></li>
+                <li><a href="#">O nás</a></li>
+                <li><a href="#">História</a></li>
+                <li><a href="#">Vedenie</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="main-container">
+        <div class="card-2">
+            <svg width="34" height="35" viewBox="0 0 34 35" fill="none" xmlns="http://www.w3.org/2000/svg" style="padding-right: 20px;">
+                <rect y="0.379883" width="34" height="34" rx="17" fill="#009ADC"/>
+                <g clip-path="url(#clip0_151_438)">
+                <g clip-path="url(#clip1_151_438)">
+                <g clip-path="url(#clip2_151_438)">
+                <path d="M16.6317 8.87988C15.1924 8.90255 13.6284 9.33322 12.7444 9.93388C12.7117 9.95264 12.6861 9.98176 12.6717 10.0166C12.6574 10.0515 12.655 10.0902 12.6651 10.1265L13.3451 11.6225C13.3561 11.6451 13.372 11.6649 13.3916 11.6806C13.4111 11.6963 13.434 11.7074 13.4584 11.7132C13.4813 11.7199 13.5054 11.7214 13.529 11.7174C13.5525 11.7135 13.5749 11.7043 13.5944 11.6905C14.2177 11.2372 15.2377 10.9539 16.1784 10.9312C17.6857 10.9312 18.5584 11.6112 18.5924 12.7899C18.6037 13.8099 17.9577 14.7392 16.8131 15.9859C15.5437 17.4139 14.9997 18.8532 15.1924 20.2925L15.2604 20.9159C15.2659 20.9569 15.2862 20.9946 15.3175 21.0217C15.3488 21.0488 15.389 21.0636 15.4304 21.0632L17.5157 21.0292C17.5594 21.0256 17.6 21.0053 17.6291 20.9725C17.6577 20.9417 17.6738 20.9013 17.6744 20.8592L17.6291 20.2245C17.5384 19.0232 17.9691 18.0032 19.0571 16.8019C20.4171 15.3285 21.3577 14.1385 21.3237 12.4952C21.2897 10.8179 20.0884 8.87988 16.7904 8.87988H16.6317ZM16.5977 22.5479C16.5562 22.5464 16.5146 22.5464 16.4731 22.5479C16.0055 22.5479 15.5556 22.7262 15.2151 23.0465C15.0567 23.2052 14.9327 23.3947 14.8508 23.6033C14.7689 23.812 14.7309 24.0352 14.7391 24.2592C14.7408 24.4782 14.7869 24.6945 14.8745 24.8951C14.9622 25.0957 15.0896 25.2765 15.2491 25.4265C15.5849 25.7273 16.0224 25.8893 16.4731 25.8799H16.5071C16.9828 25.8881 17.4428 25.709 17.7877 25.3812C17.9388 25.2255 18.0569 25.0408 18.1348 24.8382C18.2127 24.6356 18.2489 24.4194 18.2411 24.2025C18.2434 23.979 18.2004 23.7573 18.1147 23.5508C18.029 23.3444 17.9023 23.1574 17.7424 23.0012C17.4264 22.7196 17.0209 22.559 16.5977 22.5479Z" fill="white"/>
+                </g>
+                </g>
+                </g>
+                <defs>
+                <clipPath id="clip0_151_438">
+                <rect width="17" height="17" fill="white" transform="translate(8.5 8.87988)"/>
+                </clipPath>
+                <clipPath id="clip1_151_438">
+                <rect width="17" height="17" fill="white" transform="translate(8.5 8.87988)"/>
+                </clipPath>
+                <clipPath id="clip2_151_438">
+                <rect width="8.67" height="17" fill="white" transform="translate(12.665 8.87988)"/>
+                </clipPath>
+                </defs>
+                </svg>
+                
+            <div>
+                <h3>Časté otázky</h3>
+                <p>Otázky a odpovede</p>
+            </div>
+        </div>
+
+        <div class="card-2">
+            <svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect x="0.409912" y="0.379883" width="34" height="34" rx="17" fill="#009ADC"/>
+                <g clip-path="url(#clip0_151_446)">
+                <g clip-path="url(#clip1_151_446)">
+                <path d="M23.7849 20.5673C23.2891 19.9298 21.6599 18.7965 20.5266 18.7965L19.8183 19.009C19.4776 19.2645 19.0937 19.4565 18.6849 19.5757C18.5433 19.5757 18.4724 19.5757 18.1891 19.2215C17.3391 18.6548 17.1974 18.4423 16.9849 18.159L16.8433 18.0882L16.7016 17.8757L15.6391 16.7423C15.3558 16.3882 15.0724 16.1757 15.0724 15.9632L15.6391 15.0423C16.0641 14.5465 16.0641 13.7673 15.6391 12.9173C15.2815 12.2813 14.8539 11.6874 14.3641 11.1465C13.9391 10.509 13.5141 10.2965 12.9474 10.2965C12.4516 10.2965 11.8849 10.6507 11.6016 10.8632C11.1058 11.2173 10.6099 11.7132 10.4683 12.1382C10.2558 12.8465 10.1141 13.909 11.1766 15.8923C12.0974 17.6632 12.9474 18.7965 14.3641 20.1423C15.2713 21.1615 16.3211 22.0442 17.4808 22.7632C18.5428 23.6788 19.8462 24.2691 21.2349 24.4632C22.1558 24.4632 22.8641 24.4632 23.9266 23.1882C24.9183 21.984 24.4224 21.2048 23.9266 20.5673H23.7849Z" fill="white"/>
+                </g>
+                </g>
+                <defs>
+                <clipPath id="clip0_151_446">
+                <rect width="17" height="17" fill="white" transform="translate(8.90991 8.87988)"/>
+                </clipPath>
+                <clipPath id="clip1_151_446">
+                <rect width="17" height="17" fill="white" transform="translate(8.90991 8.87988)"/>
+                </clipPath>
+                </defs>
+                </svg>
+                
+                <div>
+                    <h3>+421 220 924 600</h3>
+                    <p>Denne od 8:00 do 22:00</p>
+                </div>
+            </div>
+
+        <div class="card-2">
+            <svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect x="0.879883" y="0.809814" width="34" height="34" rx="17" fill="#009ADC"/>
+                <g clip-path="url(#clip0_151_454)">
+                <g clip-path="url(#clip1_151_454)">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M17.88 18.3907C16.7112 18.3907 10.7966 13.6448 10.7966 13.6448V13.0569C10.7966 12.4761 11.2925 11.8953 11.9654 11.8953H23.7945C24.4604 11.8953 24.9633 12.4761 24.9633 13.0569V13.6448C24.9633 13.6448 19.1266 18.3907 17.88 18.3907ZM17.88 20.0623C19.1266 20.0623 24.9633 15.4794 24.9633 15.4794V22.5628C24.9633 23.1436 24.4675 23.7244 23.7945 23.7244H11.9654C11.2995 23.7244 10.7966 23.1436 10.7966 22.5628V15.4794C10.7966 15.4794 16.7112 20.0623 17.88 20.0623Z" fill="white"/>
+                </g>
+                </g>
+                <defs>
+                <clipPath id="clip0_151_454">
+                <rect width="17" height="17" fill="white" transform="translate(9.37988 9.30981)"/>
+                </clipPath>
+                <clipPath id="clip1_151_454">
+                <rect width="17" height="17" fill="white" transform="translate(9.37988 9.30981)"/>
+                </clipPath>
+                </defs>
+                </svg>
+                
+            <div>
+                <h3>Bajos.support@gmail.com</h3>
+                <p>Vašim e-mailom sa venujeme 7 dní v týždni.</p>
+            </div>
+        </div>
+
+    </div>
+    
+</body>
+</html>
